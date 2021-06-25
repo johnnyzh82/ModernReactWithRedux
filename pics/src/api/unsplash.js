0651bae498f1b64c9bd49d0ff34e39da56f37aef
Johnny.zh <johnnyzh82@gmail.com>
2021-06-25 12:55:56 -0700

    revert

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID sxncFH13GpODg0YkUXHAATEoF98m2lQOTDtSJe69N04',
+        Authorization: 'Client-ID {your_key}',
     },
 });
